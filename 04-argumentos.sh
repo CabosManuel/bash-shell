f83db29d77e0264de6b3894f0477102a6462c96e
CabosManuel <cabos.apolaya@gmail.com>
2023-11-09 22:45:09 -0500

    Argumentos

---
+++ 04-argumentos.sh
@@ -0,0 +1,11 @@
+# ! /bin/bash
+# Script de argumentos
+
+nombreCurso=$1
+horarioCurso=$2
+
+echo "Nombre del curso: $nombreCurso"
+echo "Horario: $horarioCurso"
+
+echo -e "\nNúmero de argumentos: $#"
+echo "Argumentos: $*"
